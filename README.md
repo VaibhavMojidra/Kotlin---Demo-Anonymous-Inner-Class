6cee1a740fb22e8c3d0229e306fee0ba74ea0208
VaibhavMojidra <vmojidra25@gmail.com>
2023-05-01 20:23:39 +0530

    Anonymous Inner Class

---
+++ README.md
@@ -0,0 +1,6 @@
+# Kotlin Demo Anonymous Inner Class
+
+Anonymous inner class is a pretty good concept that makes the life of a programmer very easy. Whenever we are implementing an interface, the concept of anonymous inner block comes into picture. The concept of creating an object of interface using runtime object reference is known as anonymous class.
+___
+
+[![Vaibhav Mojidra - 1.jpeg](https://raw.githubusercontent.com/VaibhavMojidra/Kotlin---Demo-Anonymous-Inner-Class/master/output/1.jpeg "Vaibhav Mojidra")](https://vaibhavmojidra.github.io/site/)
---
+++ src/Demo.kt
@@ -0,0 +1,13 @@
+//Anonymous inner class is a pretty good concept that makes the life of a programmer very easy. Whenever we are implementing an interface, the concept of anonymous inner block comes into picture. The concept of creating an object of interface using runtime object reference is known as anonymous class.
+
+fun main(args: Array<String>) {
+   var programmer :Human = object:Human { // Anonymous class
+      override fun think() { // overriding the think method
+         print("I am an example of Anonymous Inner Class ")
+      }
+   }
+   programmer.think()
+}
+interface Human {
+   fun think()
+}
